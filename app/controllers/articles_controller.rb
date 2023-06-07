cded1a295dd8b8124ecb707ab0656a44e17accca
nhson2022 <sonpgdlh@gmail.com>
2023-06-07 21:51:26 +0700

    Style bootstrap for Categories and Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.includes(:category).all
+    @categories = Category.all
   end
 
   # GET /articles/1 or /articles/1.json
@@ -58,13 +59,14 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:ten, :noi_dung, :trang_thai, :category_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:ten, :noi_dung, :trang_thai, :category_id)
+  end
 end
