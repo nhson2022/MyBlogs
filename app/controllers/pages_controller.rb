a3bf40b7b62bb7e32832211a1092f73d54e56e67
nhson2022 <sonpgdlh@gmail.com>
2023-06-08 20:24:37 +0700

    Uses template Blog apply UI for MyBlogs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @articles = Article.includes(:category).all
+    @article = Article.first
+    @article2 = Article.second
+    @article3 = Article.third
+    @categories = Category.all
   end
 
   def about
